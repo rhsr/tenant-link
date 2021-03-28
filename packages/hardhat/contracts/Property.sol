df7f90c2830b71bd5c85e2fc3c6de87a4b9cf83e
rhsr <ross@evenfinancial.com>
2021-03-28 13:27:33 -0400

    add dwelling functionality, referencing solidity structs docs (#4)
    * add dwelling functionality, referencing solidity structs docs
    
    * add link to docs
    
    Co-authored-by: Ross Richman <“ross@evenfinancial.com”>

--- packages/hardhat/contracts/Property.sol
+++ packages/hardhat/contracts/Property.sol
@@ -13,6 +13,7 @@ contract Property {
   enum VerificationStatus { AwaitingDocumentation, Pending, Approved, Rejected }
 
   //events, what can happen to a property
+  event DwellingAdded(uint dwellingId);
   event VerificationSubmitted(address sender);
   event VerificationApproved(address sender);
   event VerificationRejected(address sender);
@@ -21,7 +22,11 @@ contract Property {
   VerificationStatus verificationStatus = VerificationStatus.AwaitingDocumentation;
   string mailingAddress;
   string zipcode;
-  Dwelling[] dwellings;
+
+  //should research mem implications of a uint vs specifying the smallest one possible for the use case
+  //https://docs.soliditylang.org/en/v0.8.3/types.html#structs
+  uint numDwellings;
+  mapping (uint => Dwelling) dwellings;
 
   constructor (string memory _mailingAddress, string memory _zipcode) {
     mailingAddress = _mailingAddress;
@@ -33,9 +38,12 @@ contract Property {
     uint sqft
   ) 
     public
+    returns (uint dwellingId)
   {
-    Dwelling memory _dwelling = Dwelling(sqft);
-    dwellings.push(_dwelling);
+    dwellingId = numDwellings++;
+    Dwelling storage d = dwellings[dwellingId];
+    d.sqft = sqft;
+    emit DwellingCreated(dwellingId);
   }
 
 }
