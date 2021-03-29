c17606509cac7845064768126947cf4b74ee72af
Austin Montgomery <austin.m.montgomery@gmail.com>
2021-03-29 01:06:05 -0400

    fix format

--- packages/hardhat/contracts/Dwelling.sol
+++ packages/hardhat/contracts/Dwelling.sol
@@ -2,22 +2,22 @@ pragma solidity >=0.6.0 <0.9.0;
 // SPDX-License-Identifier: MIT
 
 struct Dwelling {
-    address tenant;
-    string identifier; //4A, 4, N, etc
-    DwellingAttributes attributes;
-    DwellingAmenities amenities;
+  address tenant;
+  string identifier; //4A, 4, N, etc
+  DwellingAttributes attributes;
+  DwellingAmenities amenities;
 }
 
 struct DwellingAttributes {
-    int16 squareFeet;
-    int8 numBedrooms;
-    int8 numBathrooms;
-    string heatingType;
-    string coolingType;
+  int16 squareFeet;
+  int8 numBedrooms;
+  int8 numBathrooms;
+  string heatingType;
+  string coolingType;
 }
 
 struct DwellingAmenities {
-    bool inUnitWasher;
-    bool inUnitDryer;
-    bool petsAllowed;
+  bool inUnitWasher;
+  bool inUnitDryer;
+  bool petsAllowed;
 }
--- packages/hardhat/contracts/PropertyFactory.sol
+++ packages/hardhat/contracts/PropertyFactory.sol
@@ -1,41 +1,32 @@
 pragma solidity >=0.6.0 <0.9.0;
 // SPDX-License-Identifier: MIT
 
-import "hardhat/console.sol";
-import "./Property.sol";
+import 'hardhat/console.sol';
+import './Property.sol';
 
 contract PropertyFactory {
-    //events
-    event PropertyCreated(address sender, string name);
+  //events
+  event PropertyCreated(address sender, string name);
 
-    //attributes
-    Property[] properties;
+  //attributes
+  Property[] properties;
 
-    function createProperty(
-        string calldata name,
-        int8 numFloors,
-        bool hasBackyard,
-        bool hasElevator,
-        string calldata streetAddress,
-        string calldata city,
-        string calldata zipcode
-    ) public propertyOwnershipVerified() {
-        Property p =
-            new Property(
-                name,
-                numFloors,
-                hasBackyard,
-                hasElevator,
-                streetAddress,
-                city,
-                zipcode
-            );
-        properties.push(p);
-        emit PropertyCreated(msg.sender, name);
-    }
+  function createProperty(
+    string calldata name,
+    int8 noFloors,
+    bool hasBackyard,
+    bool hasElevator,
+    string calldata streetAddress,
+    string calldata city,
+    string calldata zipcode
+  ) public propertyOwnershipVerified() {
+    Property p =
+      new Property(name, noFloors, hasBackyard, hasElevator, streetAddress, city, zipcode);
+    properties.push(p);
+    emit PropertyCreated(msg.sender, name);
+  }
 
-    modifier propertyOwnershipVerified() {
-        require(true, "Sender not authorized.");
-        _;
-    }
+  modifier propertyOwnershipVerified() {
+    require(true, 'Sender not authorized.');
+  }
 }
